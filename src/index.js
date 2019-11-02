fb147b9e54607c99c90d83e7f0417413a8c5ba80
Qasim Laghari <go.jets.laghari@gmail.com>
2019-11-02 13:58:06 -0700

    Update state with lat

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ class App extends React.Component {
 
     constructor(props) {
         super(props);
+        //Initialize state
         this.state = { lat:null };
+        //Set state
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({
+                    lat: position.coords.latitude
+                });
+            },
+             (err) => console.log(err)
+        );
     }
 
     render() {
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
-             (err) => console.log(err)
-        );
-        return <div>Latitude: </div>;
+        return <div>Latitude:  {this.state.lat}</div>;
     }
 }
 
